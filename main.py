918aafef4048b60a32bc65d8e389826ca56d797f
Kshitiz Sapkota <kshitiz.sapkota@edu.sait.cagit config --global user.name>
2023-10-18 15:53:47 -0600

    fully done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 #context manager
-with open ("mydefaults.ini.text") as ini_file:
+with open ("mydefaults.ini") as ini_file:
     data = ini_file.read()
+
+
+    lines = data.split ("\n")
+    key_value_ini = dict()
+    #print(len(lines))
+    for line in lines:
+        if "=" in line:
+         fields = line.split ('=')
+        key_value_ini[fields[0]] = fields[1]
+        print(key_value_ini)
+
